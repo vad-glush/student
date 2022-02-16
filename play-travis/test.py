4b7a4caad3dad6b9781c347b19490d42dd4bf122
Vadim Glushkov <vad-glush@yandex.ru>
2022-02-16 11:08:17 +0300

    init test.py

---
+++ play-travis/test.py
@@ -0,0 +1,11 @@
+--2022-02-16 11:05:57--  https://raw.githubusercontent.com/express42/otus-snippets/master/hw-04/test.py
+Распознаётся raw.githubusercontent.com (raw.githubusercontent.com)… 185.199.110.133, 185.199.108.133, 185.199.111.133, ...
+Подключение к raw.githubusercontent.com (raw.githubusercontent.com)|185.199.110.133|:443... соединение установлено.
+HTTP-запрос отправлен. Ожидание ответа… 200 OK
+Длина: 164 [text/plain]
+Сохранение в: «test.py.1»
+
+     0K                                                       100% 4,74M=0s
+
+2022-02-16 11:05:58 (4,74 MB/s) - «test.py.1» сохранён [164/164]
+
